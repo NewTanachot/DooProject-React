2702bb4332d70a1be58c1329fbf147d2dd8dae27
Nw_2000 <TanachotNew2543@gmail.com>
2023-02-14 15:55:57 +0700

    Update Login to call API Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,51 @@
 import React, { useState, FC } from "react";
 
 interface Props {
-  onLogin: (username: string, password: string) => void;
+  onLogin: (email: string, password: string) => void;
 }
 
 const Login: FC<Props> = (props) => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
   };
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    props.onLogin(username, password);
+    props.onLogin(email, password);
+    
+    const BodyData = {
+      email: email,
+      password: password
+    }
+    
+    const Response = await fetch("https://localhost:7260/api/Auth/Login", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(BodyData),
+    });
+
+    const Data = await Response.json();
+    
+    // Save UserId and another credential to Localstorage
+    // console.log(Data);
+    localStorage.setItem("JWT", JSON.stringify(Data));
+
+    var a = localStorage.getItem("JWT");
+
+    if (a != null){
+      var obj = JSON.parse(a);
+    }
+
+    console.log(obj);
   };
 
   return (
@@ -28,14 +55,14 @@ const Login: FC<Props> = (props) => {
         className="flex flex-col justify-start items-center w-96 md:w-1/3 bg-white p-4 mt-8 rounded-lg shadow-md"
       >
         <div className="w-full mb-4">
-          <label htmlFor="username" className="block font-bold mb-2">
-            Username:
+          <label htmlFor="email" className="block font-bold mb-2">
+            Email:
           </label>
           <input
             type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
             className="input input-bordered w-full"
           />
         </div>
